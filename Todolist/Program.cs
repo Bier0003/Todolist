5b3d3bbc66cff24262ff60a4ba60bb672199979b
Pornpan Madsen <bier0003@study.sde.dk>
2024-01-17 08:40:21 +0100

    Add project files.

---
+++ Todolist/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Todolist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Todo>
+            {
+                new Todo
+                {
+                    TodoText = "Go to zoo"
+                }
+            };
+            list.Add(new Todo { TodoText = "Go to thailand" });
+
+            Console.WriteLine("Velkommen til TodoList Apps");
+            Console.WriteLine("Vælge dn mulighed");
+            Console.WriteLine("1) Tilføje liste");
+            Console.WriteLine("2) Slet liste");
+            Console.WriteLine("3) View liste");
+            Console.WriteLine("4) Markeres som afsluttet");
+            Console.WriteLine("E)Exit");
+
+            String userInput = Console.ReadLine();
+
+            Console.WriteLine("Du gerne:" + userInput + "\n");
+
+
+        }
+    }
+}
---
+++ Todolist/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todolist
+{
+    internal class Todo
+    {
+        public string TodoText { get; set; }
+        public bool Checked { get; set; }
+    }
+}
